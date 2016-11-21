778e23a5c02b1eba6c57feb0ee9d684d7d85c70f
May Jawdat <may.jawdat@gmail.com>
2016-11-20 22:14:55 -0800

    Add if statements

--- ruby/vampires.rb
+++ ruby/vampires.rb
@@ -9,16 +9,41 @@
 
 puts "What is your name?"
 name1 = gets.chomp
+if name1 == "Drake Cula"
+  name1 = false
+elsif name1 == "Tu Fang"
+  name1 = false
+else
+  name1 = true
+end
+
 
 puts "How old are you?"
 age = gets.chomp
 
 puts "What year were you born?"
 birth_year = gets.chomp
+age_calc = 2016 - birth_year.to_i
+if age_calc == age
+  age = true
+else
+  age = false
+end
+
 
 puts "Our company cafeteria serves garlic bread. Should we order some for you? (y/n)"
 bread = gets.chomp
+if bread == "y"
+  bread = true
+else
+  bread = false
+end
 
 puts "Would you like to enroll in the company’s health insurance? (y/n)"
 insurance = gets.chomp
+if insurance == "y"
+  insurance = true
+else
+  insurance = false
+end
 
