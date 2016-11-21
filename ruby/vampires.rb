99b0c725d8a97f639ed5922a4acc5dea3eb78bd6
May Jawdat <may.jawdat@gmail.com>
2016-11-20 21:12:39 -0800

    Add intake questions

---
+++ ruby/vampires.rb
@@ -0,0 +1,24 @@
+# ask for name
+# ask for age
+# ask for year born
+# garlic bread y/n
+# health insurance y/n
+
+
+
+
+puts "What is your name?"
+name1 = gets.chomp
+
+puts "How old are you?"
+age = gets.chomp
+
+puts "What year were you born?"
+birth_year = gets.chomp
+
+puts "Our company cafeteria serves garlic bread. Should we order some for you? (y/n)"
+bread = gets.chomp
+
+puts "Would you like to enroll in the company’s health insurance? (y/n)"
+insurance = gets.chomp
+
