935eb9fac1724afc87185c140cd8266359623944
May Jawdat <may.jawdat@gmail.com>
2016-11-25 23:40:35 -0800

    Add pseudocode

---
+++ ruby/hashes.rb
@@ -0,0 +1,8 @@
+
+
+# Create hash: name, age, number of children, decor theme, and so on
+# Ask for answers
+# Print hash w/answers
+# Ask what category to update (key or none)
+# Print updated version of hash
+# Exit
