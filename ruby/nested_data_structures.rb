b2241f8c88f8ec5b9db349835a1d08a9fa9d6d1a
May Jawdat <may.jawdat@gmail.com>
2016-11-26 21:31:10 -0800

    Initial commit w/nested data structure framework

---
+++ ruby/nested_data_structures.rb
@@ -0,0 +1,35 @@
+# Unicorn State College Fall Semester
+# Departments: Horn Management, Glitter Studies, History of Rainbows, Friendship Science
+
+
+
+
+USCFall2016 = {
+  horn_management: {
+    department: ,
+    classes: ,
+    professors: ,
+    students: 
+  }
+
+  glitter_studies: {
+    department: ,
+    classes: ,
+    professors: ,
+    students: 
+  }
+
+  history_of_rainbows: {
+    department: ,
+    classes: ,
+    professors: ,
+    students: 
+  }
+
+  friendship_science: {
+    department: ,
+    classes: ,
+    professors: ,
+    students: 
+  }
+}
