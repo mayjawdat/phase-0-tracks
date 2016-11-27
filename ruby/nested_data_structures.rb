cd1c9c6446e87be16c8b15fa66936d19f9aeca5d
May Jawdat <may.jawdat@gmail.com>
2016-11-26 22:15:35 -0800

    Complete data input

--- ruby/nested_data_structures.rb
+++ ruby/nested_data_structures.rb
@@ -1,35 +1,79 @@
-# Unicorn State College Fall Semester
-# Departments: Horn Management, Glitter Studies, History of Rainbows, Friendship Science
-
+# Unicorn State College Fall Semester Course Catalog
+# Departments: strings
+# Classes: array of strings
+# Professors: array of strings
+# Student Info: hash of integers
 
 
 
 USCFall2016 = {
   horn_management: {
-    department: ,
-    classes: ,
-    professors: ,
-    students: 
+    department: "Horn Management" ,
+    classes: [
+      "Intro to Protrusions",
+      "Statistics",
+      "Advanced Horn Upkeep"
+    ],
+    professors:[
+      "Dr. Lollipop",
+      "Dr. Candy Cane"
+    ],
+    student_info: {
+      majors: 12,
+      minors: 3
+    }
   }
 
   glitter_studies: {
-    department: ,
-    classes: ,
-    professors: ,
-    students: 
+    department: "Glitter Studies",
+    classes: [
+      "Shiny vs. Sparkley",
+      "Festive Uses of Glitter Post-WWII",
+    ],
+    professors: [
+      "Dr. Bubblegum",
+      "Dr. Jelly Bean"
+    ],
+    student_info: {
+      majors: 4,
+      minors: 5
+    }
   }
 
   history_of_rainbows: {
-    department: ,
-    classes: ,
-    professors: ,
-    students: 
+    department: "History of Rainbows",
+    classes: [
+      "Primary Colors 101",
+      "Roy G. Biv, American Icon",
+      "Controversies in History of Rainbows - Pink",
+      "Intermediate Spectrum Seminar"
+    ],
+    professors: [
+      "Dr. Kittens",
+      "Dr. Licorice",
+      "Dr. Cotton Candy"
+    ],
+    student_info: {
+      majors: 14,
+      minors: 6
+    }  
   }
 
   friendship_science: {
-    department: ,
-    classes: ,
-    professors: ,
-    students: 
+    department: "Friendship Science",
+    classes: [
+      "Gender and Friendship - Sleepovers 201",
+      "Understanding Grumpiness",
+      "Turning That Frown Upside Down"
+    ],
+    professors: [
+      "Dr. Fluffy Clouds",
+      "Dr. Overwhelming Exuberance",
+      "Dr. Tickles"
+    ],
+    student_info: {
+      majors: 8,
+      minors: 3
+    }
   }
 }
