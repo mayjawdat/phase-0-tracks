5a164d5ca13122cce8dae3e32742a5ca9618e7ba
May Jawdat <may.jawdat@gmail.com>
2016-11-28 04:31:30 -0800

    I have no idea so I just put a bunch of stuff in that seemed like it would probably be useful in some way

--- ruby/alias_manager.rb
+++ ruby/alias_manager.rb
@@ -3,7 +3,7 @@
 # reverse
 # put back together
 # downcase
-# set vowel hash
+# set vowel hash?
 # find vowels
 # if vowels replace with next item in array
 # set consonant array
@@ -12,23 +12,38 @@
 
 vowels = ["a","e","i","o","u"]
 string = "Joe Smith"
-string.split(' ').reverse.join('').downcase!
-
-def translate_char(char)
-vowels = "aeiou"
-if vowels.index(char)
-  char
-end
-
-def translate_name(name)
+char = string.split('').reverse.downcase
 #  name.split(' ').reverse.join('').downcase!
 
-  char_index = 0
-  updated_response = ""
-  while char_index < name.length
-    updated_response << translate_char(name[char_index])
-    char_index += 1
-  end
-  updated_response
+def translate_char(char)
+char.split('').reverse.downcase
+vowels = ["a","e","i","o","u"]
+# something in here to see if any of the elements of char are in the vowels array
+# if so find out the index of the element in the vowels array
+# convert index to integer
+# add 1
+# get vowel at new index
+# new vowel = vowels.values_at("#{vowel}")
 end
 
+# or possibly vowels = {a: e, e: i, i: o, o: u, u: a}
+# vowels[vowel input]
+
+def translate_name(name)
+  char_index = 0
+  while char_index < name.length
+    puts name[char_index]
+    char_index += 1
+  end
+end
+
+# user interface
+puts "What is your real name?"
+real_name = gets.chomp
+until real_name == "quit"
+puts spy_name(real_name)
+end
+
+final_list.each {|spy_name,real_name|
+puts "#{spy_name} is actually #{real_name}"
+}
