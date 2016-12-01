055e6d772103cbb68f4618c2cf79657d57933c36
May Jawdat <may.jawdat@gmail.com>
2016-11-30 23:42:39 -0800

    Add puppy details

---
+++ ruby/puppy.rb
@@ -0,0 +1,18 @@
+=begin
+
+Species
+Puppulus Flopsimus
+
+Characteristics
+Eyes: large and round
+Ears: soft and floppy
+Thirst for blood: insatiable
+Weight: 10-20 lbs
+
+
+Behavior
+Barking
+Napping
+Chasing its own tail
+
+=end
