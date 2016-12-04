31e63a6282ec7d5cc29898945785517080ff8785
May Jawdat <may.jawdat@gmail.com>
2016-12-04 02:26:23 -0800

    Add getter and setter methods, driver code

--- ruby/santa.rb
+++ ruby/santa.rb
@@ -5,35 +5,37 @@ class Santa
     @gender = gender
     @ethnicity = ethnicity
     reindeer = ["Rudolph", "Dasher", "Dancer", "Prancer", "Vixen", "Comet", "Cupid", "Donner", "Blitzen"]
-    @reindeer_ranking = reindeer.index
+    @reindeer_ranking = reindeer
     @age = 0
   end
 
-  def speak
-    puts "Ho, ho, ho! Haaaappy holidays!"
+# getter methods
+  def age
+    @age
   end
 
-  def eat_milk_and_cookies(cookie_type)
-    puts "That was a good #{cookie_type}!"
+  def ethnicity
+    @ethnicity
   end
 
+# setter method
+  def gender=(new_gender)
+   @gender = new_gender
+  end
+
+ def celebrate_age
+   @age = age + 1
+ end
+
+  def get_mad_at(bad_reindeer)
+   @reindeer_ranking = reindeer.insert(-1,bad_reindeer)
+  end
 end
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+phil = Santa.new("female","beige")
+phil.age
+# can't figure out how to add in age initially
+p phil
+phil.gender=("lady")
+phil.celebrate_age
+p phil
