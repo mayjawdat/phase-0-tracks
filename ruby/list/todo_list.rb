a126caefddc7eb5287355e45a371c1287c5ac64e
May Jawdat <may.jawdat@gmail.com>
2016-12-04 12:42:52 -0800

    Initial rspec attempts

---
+++ ruby/list/todo_list.rb
@@ -0,0 +1,18 @@
+class TodoList
+
+  def get_items
+    list = []
+  end
+  
+  def add_items(item)
+    list << item
+  end
+  
+  def delete_item(item)
+    list.delete(item)
+  end
+
+  def get_item(n)
+  end
+
+end
--- ruby/list/todo_list_spec.rb
+++ ruby/list/todo_list_spec.rb
@@ -1,8 +1,11 @@
+require_relative 'todo_list'
+
+
 describe TodoList do
   let(:list) { TodoList.new(["do the dishes", "mow the lawn"]) }
 
   it "stores the list items given on initialization" do
-    expect(list.get_items).to eq ["do the dishes", "mow the lawn"]
+    expect(list.get_items("do the dishes", "mow the lawn")).to eq ["do the dishes", "mow the lawn"]
   end
 
   it "adds an item to the list" do
