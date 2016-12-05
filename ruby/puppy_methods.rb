19406d5f968c82d5af6edd6276623cf5e4febdeb
May Jawdat <may.jawdat@gmail.com>
2016-12-04 20:50:42 -0800

    Add lion class and .each iterations

--- ruby/puppy_methods.rb
+++ ruby/puppy_methods.rb
@@ -1,4 +1,7 @@
 class Puppy
+  def initialize
+    puts "Initializing new puppy instance..."
+  end
 
   def fetch(toy)
     puts "I brought back the #{toy}!"
@@ -17,13 +20,36 @@ class Puppy
     x * 7
   end
 
+end
 
 fancy = Puppy.new
-
 
 p fancy.fetch("ball")
 p fancy.speak(3)
 p fancy.roll_over
 p fancy.dog_years(4)
 
+class Lion
+  def initialize
+      puts "Initializing new lion instance..."
+  end
+  
+  def roar(num)
+    puts "Roar!" * num
+  end
+  
+  def jump
+    puts "Jump!"
+  end
 end
+
+
+lion_array = []
+while lion_array.count < 50
+lion_array << Lion.new
+end
+
+lion_array.each {|x| x.roar(2)}
+lion_array.each {|x| x.jump}
+
+
