00115cf29c1d2cd204dd12fd9bd2c8daaac5a6f6
May Jawdat <may.jawdat@gmail.com>
2016-12-10 14:31:17 -0800

    Add reverse string function

--- js/explore.js
+++ js/explore.js
@@ -1,4 +1,16 @@
-Break down string into individual letters
-For each letter in string, put before previous letter in new string
+//Break down string into individual letters
+//For each letter in string, put before previous letter in new string
+//original string = [letter1][letter2][letter3][letter4]
+//new string = [letter1]
+//  = [letter2] + [letter1]
+//  = [letter3] + [letter2][letter1]
+//  = [letter4] + [letter3][letter2][letter1]
 
-for (var i = )
+
+var str = "the house on the hill";
+
+newString =""
+for (var i = str.length - 1; i >= 0; i--){
+  newString = newString + str[i];
+}
+console.log(newString);
