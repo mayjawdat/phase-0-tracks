ef74641b1314b3c6dda7ce3e4f5407a3816dd6d2
May Jawdat <may.jawdat@gmail.com>
2016-12-12 02:24:28 -0800

    Release 0 attempts

---
+++ js/algos.js
@@ -0,0 +1,19 @@
+
+//Release 0
+//array = [phrase1, phrase2, phrase3]
+//count letters in each phrase
+//sort largest to smallest - this is the part I can't figure out
+//return phrase in 1st position
+
+var array = ["cats", "apples", "waterfalls"];
+
+
+var longestWord = function(array) {
+for (var i = 0; i < array.length; i++){
+  array[i].length;
+  return(array[0]);
+}
+};
+
+
+//Release 1
