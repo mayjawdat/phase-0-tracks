2bafff3e5392d3712efd662b1da0cb07c5e544f1
May Jawdat <may.jawdat@gmail.com>
2016-12-17 21:44:09 -0800

    Add initial hash

---
+++ ruby/hashes.rb
@@ -0,0 +1,16 @@
+# Create hash: name, age, number of children, decor theme, and so on
+# Ask for answers
+# Print hash w/answers
+# Ask what category to update (key or none)
+# Print updated version of hash
+# Exit
+
+
+designer_form = {
+  :name => [],
+  :age => [],
+  :no_of_children => [],
+  :decor_theme => [],
+  :rooms_count => [],
+  :deposit_paid => []
+}
