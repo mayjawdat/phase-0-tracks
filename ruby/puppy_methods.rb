a4e0d67d78eda3478e7b312964154481e00c23c3
May Jawdat <may.jawdat@gmail.com>
2016-12-30 20:07:52 -0800

    Add puppy methods, release 0

--- ruby/puppy_methods.rb
+++ ruby/puppy_methods.rb
@@ -5,5 +5,23 @@ class Puppy
     toy
   end
 
+  def speak(num)
+    num.times {|x| puts "Woof!"}
+  end
+
+  def roll_over
+    puts "*rolls over*"
+  end
+
+  def dog_years(human_years)
+    human_years * 7
+  end
+
+
 end
 
+frida = Puppy.new
+p frida.fetch("ball")
+frida.speak(2)
+frida.roll_over
+p frida.dog_years(4)
