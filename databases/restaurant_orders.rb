060fb25a5fc6fe8ed600ec642d701e9364dffdd6
May Jawdat <may.jawdat@gmail.com>
2017-01-16 00:19:04 -0800

    Initial pseudocode

---
+++ databases/restaurant_orders.rb
@@ -0,0 +1,32 @@
+
+=begin
+
+Daily inventory order
+Suppliers table
+- Supplier
+- Item
+- Unit price
+- Total
+- Contact info
+
+Menu
+- Dishes
+- Ingredients
+- Quantities per ingredient
+
+
+Covers
+- Dishes ordered * quantities per ingredient = amount sold
+- Dishes ordered as % of total ordered
+- Reservations for next service
+- % * covers -> # of each dish -> * ingredients -> amount needed
+
+amount needed + amount sold = amount to order
+
+User inputs
+total covers today
+# of dishes sold
+reservations for tomorrow
+
+Back end input
+amount to order maps to supplier list (nested hash)
