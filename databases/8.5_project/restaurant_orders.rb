c6649e634e9f30e2c5d9b6bcd96ee73d08acaf59
May Jawdat <may.jawdat@gmail.com>
2017-01-16 02:42:48 -0800

    Update hashes, restaurant database tables

--- databases/8.5_project/restaurant_orders.rb
+++ databases/8.5_project/restaurant_orders.rb
@@ -1,9 +1,66 @@
+require 'sqlite3'
+
 
 db = SQLite3::Database.new("restaurant.db")
 # db.results_as_hash = true
 
-create_table_cmd = <<-SQL
-  #CREATE TABLE IF NOT EXISTS ingredients(
+create_table_pasta = <<-SQL
+  CREATE TABLE IF NOT EXISTS pasta(
+    id INTEGER PRIMARY KEY,
+    name VARCHAR(255),
+    unit_price INT
+  )
+SQL
+
+create_table_sauce = <<-SQL
+  CREATE TABLE IF NOT EXISTS sauce(
+    id INTEGER PRIMARY KEY,
+    name VARCHAR(255),
+    unit_price INT
+  )
+SQL
+
+create_table_cheese = <<-SQL
+  CREATE TABLE IF NOT EXISTS cheese(
+    id INTEGER PRIMARY KEY,
+    name VARCHAR(255),
+    unit_price INT
+  )
+SQL
+  
+pasta_hash = {
+  name: ["penne", "rotini", "pappardelle"],
+  unit_price: [1, 1.5, 2]
+}
+
+sauce_hash = {
+  name: ["bolognese", "pesto", "alfredo"],
+  unit_price: [2, 1, 1.5]
+}
+
+cheese_hash = {
+  name: ["mozzarella", "parmesan", "fontina"],
+  unit_price: [2, 1, 1.5]
+}
+
+
+def update_table(table, name, unit_price)
+  db.execute("INSERT INTO #{table} (name, unit_price) VALUES (?, ?)", [name, unit_price])
+end
+
+ingredients_array = [pasta_hash, sauce_hash, cheese_hash]
+
+ingredients_array.each do |table, name, unit_price|
+  INSERT INTO table (name, unit_price) VALUES (?, ?), [name, unit_price]
+end
+
+db.execute(create_table_pasta)
+db.execute(create_table_sauce)
+db.execute(create_table_cheese)
+
+update_table("pasta", pasta_hash[:name], pasta_hash[:unit_price])
+
+#CREATE TABLE IF NOT EXISTS ingredients(
   #  id INTEGER PRIMARY KEY,
   #  pasta_id int,
   #  sauce_id int,
@@ -12,29 +69,6 @@ create_table_cmd = <<-SQL
   #  FOREIGN KEY (sauce_id) REFERENCES sauce(id),
   #  FOREIGN KEY (cheese_id) REFERENCES cheese(id),
   #)
-
-  CREATE TABLE IF NOT EXISTS pasta(
-    id INTEGER PRIMARY KEY,
-    name VARCHAR(255),
-    unit_price INTEGER
-  )
- 
-  CREATE TABLE IF NOT EXISTS sauce(
-    id INTEGER PRIMARY KEY,
-    name VARCHAR(255),
-    unit_price INTEGER
-  )
-
-  CREATE TABLE IF NOT EXISTS cheese(
-    id INTEGER PRIMARY KEY,
-    name VARCHAR(255),
-    unit_price INTEGER
-  )
-
-SQL
-
-
-
 
 
 
@@ -93,3 +127,4 @@ suppliers = {
   }
   :contact_info
 }
+=end
